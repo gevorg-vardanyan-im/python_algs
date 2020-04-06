96e665458a08f69e7bfda528f553cd8b8053d3dd
Gevorg Vardanyan <gevorg_v@instigatemobile.com>
2020-04-06 04:34:50 +0400

    Updated doubly_linked_list structure.

--- data_structures/linked_list/doubly_linked_list.py
+++ data_structures/linked_list/doubly_linked_list.py
@@ -1,13 +1,3 @@
-'''
-- A linked list is similar to an array, it holds values. However, links in a linked list do not have indexes.
-- This is an example of a double ended, doubly linked list.
-- Each link references the next link and the previous one.
-- A Doubly Linked List (DLL) contains an extra pointer, typically called previous pointer,
-together with next pointer and data which are there in singly linked list.
- - Advantages over SLL - IT can be traversed in both forward and backward direction.,Delete operation is more efficent'''
-from __future__ import print_function
-
-
 class Node:
     next = None  # This points to the link in front of the new node
     previous = None  # This points to the link behind the new node
