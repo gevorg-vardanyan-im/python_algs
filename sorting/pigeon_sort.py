2674edd2fa3e99bd0e39eef3b63039879f8c1a88
Gevorg Vardanyan <gevorg_v@instigatemobile.com>
2020-04-12 22:20:56 +0400

    Added pigeonhole sorting alg.

--- sorting/pigeon_sort.py
+++ sorting/pigeon_sort.py
@@ -1,5 +1,19 @@
 """
-    This is an implementation of Pigeon Hole Sort.
+    Pigeonhole sorting is a sorting algorithm that is suitable for sorting lists
+    of elements where the number of elements (n) and the length of the
+    range of possible key values (N) are approximately the same.
+    Worst-case performance	O ( N + n ) ,
+        where N is the range of key values and n is the input size.
+
+    The pigeonhole algorithm works as follows:
+    1.  Given an array of values to be sorted, set up an auxiliary array of
+        initially empty "pigeonholes," one pigeonhole for each key through
+        the range of the original array.
+    2.  Going over the original array, put each value into the pigeonhole
+        corresponding to its key, such that each pigeonhole eventually
+         contains a list of all values with that key.
+    3.  Iterate over the pigeonhole array in order, and put elements from
+        non-empty pigeonholes back into the original array.
 """
 
 
@@ -33,7 +47,7 @@ def pigeon_sort(array):
 
 if __name__ == '__main__':
     import random
-    random_list = random.sample(range(0, 100), 10)
-    random.shuffle(random_list)
+
+    random_list = [random.randint(14,20) for x in range(20)]
     print('Random list:', random_list)
     print('Sorted list:', pigeon_sort(random_list))
